8c26c08ae474add539cb59ec39cf9b4929c0f3a2
Vivek428 <vivekjha23012001@gmail.com>
2025-02-20 06:35:01 +0000

    conversionOeration

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,2 @@
+console.log("hello every one my self vivek kumar");
+console.log("pam pama pam");
---
+++ 01_basics/02_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "33";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+
+//"33" => 33
+//"33abc" => NaN(note a Number)
+//"true" => 1,"false" => 0
+
+let isLoogedIn = "vivek";
+let booleanIsLoogedIn = Boolean(isLoogedIn);
+console.log(booleanIsLoogedIn);
+
+//"" => false
+//"vivek" => true
